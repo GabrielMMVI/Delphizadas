f9189e184dc3096645ed35b511ed8d41a4b77e6a
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-12-23 08:02:52 -0300

    Alterações no projeto

--- Estoque/Estoque.dpr
+++ Estoque/Estoque.dpr
@@ -6,7 +6,13 @@ uses
   Main in 'Main.pas' {frmMain},
   Login in 'Login.pas' {frmLogin},
   Consulta in 'Consulta.pas' {fConsulta},
-  Adicionar in 'Adicionar.pas' {fAdicionar};
+  Adicionar in 'Adicionar.pas' {fAdicionar},
+  Alterar in 'Alterar.pas' {fAlterar},
+  Excluir in 'Excluir.pas' {fExcluir},
+  Imprimir in 'Imprimir.pas' {fImprimir},
+  Sair in 'Sair.pas' {fSair},
+  Ajuda in 'Ajuda.pas' {fAjuda},
+  DataModule in 'DataModule.pas' {Banco: TDataModule};
 
 {$R *.res}
 
@@ -17,5 +23,11 @@ begin
   Application.CreateForm(TfrmLogin, frmLogin);
   Application.CreateForm(TfConsulta, fConsulta);
   Application.CreateForm(TfAdicionar, fAdicionar);
+  Application.CreateForm(TfAlterar, fAlterar);
+  Application.CreateForm(TfExcluir, fExcluir);
+  Application.CreateForm(TfImprimir, fImprimir);
+  Application.CreateForm(TfSair, fSair);
+  Application.CreateForm(TfAjuda, fAjuda);
+  Application.CreateForm(TBanco, Banco);
   Application.Run;
 end.
